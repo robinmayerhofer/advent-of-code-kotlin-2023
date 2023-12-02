0f42be1036df4c0c3b0d570c4da38e9c089c669e
Robin Mayerhofer <32525368+rm98@users.noreply.github.com>
2023-12-02 09:24:59 +0100

    Day03 Preparation

---
+++ src/Day03.kt
@@ -0,0 +1,25 @@
+
+fun main() {
+
+    fun part1(input: List<String>): Int =
+            input.sumOf { 
+                it.length
+            }
+
+    fun part2(input: List<String>): Int =
+            input.sumOf {
+                it.length
+            }
+
+
+    // test if implementation meets criteria from the description, like:
+    val testInput = readInput("Day03_test")
+    check(part1(testInput) == 1)
+
+    val input = readInput("Day03")
+    part1(input).println()
+
+     val testInput2 = readInput("Day03_test02")
+     check(part2(testInput2) == 1)
+    part2(input).println()
+}
