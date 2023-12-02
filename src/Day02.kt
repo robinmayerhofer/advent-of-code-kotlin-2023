970a2bb034b79cfaf6237ec0f873b28a0768578d
Robin Mayerhofer <32525368+rm98@users.noreply.github.com>
2023-12-02 09:18:48 +0100

    Day02 refactoring

--- src/Day02.kt
+++ src/Day02.kt
@@ -1,70 +1,64 @@
 import Color.*
 
 enum class Color {
-    blue,
-    green,
-    red
+    BLUE,
+    GREEN,
+    RED,
 }
 
+private val gameRegex = "Game (\\d+)".toRegex()
+private val greenRegex = "(\\d+) green".toRegex()
+private val redRegex = "(\\d+) red".toRegex()
+private val blueRegex = "(\\d+) blue".toRegex()
 
 fun main() {
+    fun findGameNumber(line: String): Int =
+        gameRegex.find(line)!!.groupValues[1].toInt()
+
+    fun adaptMaxFromSet(maxValues: MutableMap<Color, Int>, set: String) {
+        val green = greenRegex.find(set)?.groupValues?.lastOrNull()?.toInt() ?: 0
+        val red = redRegex.find(set)?.groupValues?.lastOrNull()?.toInt() ?: 0
+        val blue = blueRegex.find(set)?.groupValues?.lastOrNull()?.toInt() ?: 0
+
+        maxValues.merge(RED, red, ::maxOf)
+        maxValues.merge(BLUE, blue, ::maxOf)
+        maxValues.merge(GREEN, green, ::maxOf)
+    }
+
+    fun findMaxColorValuesInLine(line: String): Map<Color, Int> {
+        val maxValues = mutableMapOf<Color, Int>()
+        val (game, sets) = line.split(":")
+
+        sets.split(";").forEach { set ->
+            adaptMaxFromSet(maxValues, set)
+        }
+
+        return maxValues
+    }
+
     fun part1(input: List<String>): Int =
-            input.map { line ->
+            input.sumOf { line ->
                 val limit = mapOf(
-                        red to 12,
-                        green to 13,
-                        blue to 14,
+                        RED to 12,
+                        GREEN to 13,
+                        BLUE to 14,
                 )
 
-                val maxValues = mutableMapOf<Color, Int>()
+                val gameNumber = findGameNumber(line)
+                val maxValues = findMaxColorValuesInLine(line)
 
-                val (game, sets) = line.split(":")
-
-                val gameNumber = "Game (\\d+)".toRegex().find(game)!!.groupValues[1].toInt()
-
-                sets.split(";").forEach { set ->
-                    println("Set: $set")
-                    val green = "(\\d+) green".toRegex().find(set)?.groupValues?.lastOrNull()?.toInt() ?: 0
-                    val red = "(\\d+) red".toRegex().find(set)?.groupValues?.lastOrNull()?.toInt() ?: 0
-                    val blue = "(\\d+) blue".toRegex().find(set)?.groupValues?.lastOrNull()?.toInt() ?: 0
-
-                    maxValues.merge(Color.red, red, ::maxOf)
-                    maxValues.merge(Color.blue, blue, ::maxOf)
-                    maxValues.merge(Color.green, green, ::maxOf)
-                }
-
-                for (limitEntry in limit) {
-                    if (limitEntry.value < (maxValues[limitEntry.key] ?: 0)) {
-                        println("Game is NOT possible: $gameNumber")
-                        return@map 0
+                gameNumber.takeIf {
+                    limit.all {  limitEntry ->
+                        limitEntry.value >= (maxValues[limitEntry.key] ?: 0)
                     }
-                }
-                println("Game is possible: $gameNumber, $maxValues")
-                return@map gameNumber
-            }.sum()
+                } ?: 0
+            }
 
     fun part2(input: List<String>): Int =
-            input.map { line ->
-
-                val maxValues = mutableMapOf<Color, Int>()
-
-                val (game, sets) = line.split(":")
-
-                val gameNumber = "Game (\\d+)".toRegex().find(game)!!.groupValues[1].toInt()
-
-                sets.split(";").forEach { set ->
-                    println("Set: $set")
-                    val green = "(\\d+) green".toRegex().find(set)?.groupValues?.lastOrNull()?.toInt() ?: 0
-                    val red = "(\\d+) red".toRegex().find(set)?.groupValues?.lastOrNull()?.toInt() ?: 0
-                    val blue = "(\\d+) blue".toRegex().find(set)?.groupValues?.lastOrNull()?.toInt() ?: 0
-
-                    maxValues.merge(Color.red, red, ::maxOf)
-                    maxValues.merge(Color.blue, blue, ::maxOf)
-                    maxValues.merge(Color.green, green, ::maxOf)
-                }
-
+            input.sumOf { line ->
+                val maxValues = findMaxColorValuesInLine(line)
                 maxValues.values.reduce { a, b -> a * b }
-            }.sum()
+            }
 
 
     // test if implementation meets criteria from the description, like:
@@ -75,6 +69,7 @@ fun main() {
     check(part2(testInput2) == 2286)
 
     val input = readInput("Day02")
+
     part1(input).println()
     part2(input).println()
 }
