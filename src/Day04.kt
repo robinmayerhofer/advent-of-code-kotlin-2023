c86c7dce7db98cada97120a957395810c5c71604
Robin Mayerhofer <32525368+rm98@users.noreply.github.com>
2023-12-04 06:51:04 +0100

    Day04 Part 1

--- src/Day04.kt
+++ src/Day04.kt
@@ -1,8 +1,21 @@
+import kotlin.math.pow
+
 fun main() {
 
+    val numberRegex = "(\\d+)".toRegex()
+
     fun part1(input: List<String>): Int =
-            input.sumOf {
-                it.length
+            input.sumOf { line ->
+                val (winning, actual) = line.split(":")[1].split("|")
+                val winningSet = numberRegex.findAll(winning).map { it.groups.first()!!.value.toInt() }.toSet()
+                val actualSet = numberRegex.findAll(actual).map { it.groups.first()!!.value.toInt() }.toSet()
+
+                val size = winningSet.intersect(actualSet).size
+                if (size == 0) {
+                    0
+                } else {
+                    2.toDouble().pow((size - 1).toDouble()).toInt()
+                }
             }
 
     fun part2(input: List<String>): Int =
@@ -14,7 +27,7 @@ fun main() {
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day04_test")
     val testOutput = part1(testInput)
-    val expectedTestOutput = 1
+    val expectedTestOutput = 13
     check(testOutput == expectedTestOutput) {
         "Part 1 Tests: Expected $expectedTestOutput, got $testOutput"
     }
@@ -22,9 +35,8 @@ fun main() {
     val input = readInput("Day04")
     part1(input).println()
 
-    val testInput2 = readInput("Day04_test2")
-    val testOutput2 = part2(testInput2)
-    val expectedTestOutput2 = 1
+    val testOutput2 = part2(testInput)
+    val expectedTestOutput2 = 30
     check(testOutput2 == expectedTestOutput2) {
         "Part 2 Tests: Expected $expectedTestOutput2, got $testOutput2"
     }
