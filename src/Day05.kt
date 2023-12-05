72fc0b89880baffe914a6c5634b4008388f2e3d0
Robin Mayerhofer <32525368+rm98@users.noreply.github.com>
2023-12-05 07:44:53 +0100

    Day05 Part 2

--- src/Day05.kt
+++ src/Day05.kt
@@ -57,8 +57,6 @@ fun main() {
                 }
         }
 
-        println(maps)
-
         return seeds.minOf { seed ->
             var value = seed
             for (map in maps) {
@@ -69,7 +67,58 @@ fun main() {
     }
 
     fun part2(input: List<String>): Long {
-        return 1
+        var seedNumbers = numberRegex.findAll(input[0].split(":")[1]).map { it.groups[0]!!.value.toLong() }.toList()
+        val seeds = mutableListOf<LongRange>()
+        while (seedNumbers.isNotEmpty()) {
+            seeds.add(
+                seedNumbers[0]..<seedNumbers[0] + seedNumbers[1]
+            )
+            seedNumbers = seedNumbers.drop(2)
+        }
+
+        val seedCount = seeds.sumOf { it.count() }
+        println("Seed count: $seedCount")
+
+        val maps = mutableListOf<XtoYMap>()
+
+        for (line in input.drop(1)) {
+            if (line.isBlank()) continue
+
+            mapNameRegex.find(line)?.groups?.lastOrNull()?.let { group ->
+                maps.add(
+                    XtoYMap(
+                        name = group.value
+                    )
+                )
+            }
+
+            rangesRegex.findAll(line).toList()
+                .takeIf { it.firstOrNull()?.groups?.size == 4 }
+                ?.let { matchResults ->
+                    val matchResult = matchResults[0]
+                    val destinationRangeStart = matchResult.groups[1]!!.value.toLong()
+                    val sourceRangeStart = matchResult.groups[2]!!.value.toLong()
+                    val length = matchResult.groups[3]!!.value.toLong()
+
+                    maps.last().rangeMappings.add(
+                        RangeMapping(
+                            sourceRange = LongRange(sourceRangeStart, sourceRangeStart + length - 1),
+                            destinationRange = LongRange(destinationRangeStart, destinationRangeStart + length - 1),
+                        )
+                    )
+                }
+        }
+
+        return seeds.asSequence().flatMap { it.asSequence() }.mapIndexed { index, seed ->
+            if (index % 10_000 == 0) {
+                println("Progress: ${(index.toDouble()/seedCount.toDouble() * 100)}%")
+            }
+            var value = seed
+            for (map in maps) {
+                value = map.mapValue(value)
+            }
+            value
+        }.min()
     }
 
     // test if implementation meets criteria from the description, like:
@@ -83,11 +132,12 @@ fun main() {
     val input = readInput("Day05")
     part1(input).println()
 
-    val testInput2 = readInput("Day05_test2")
-    val testOutput2 = part2(testInput2)
-    val expectedTestOutput2 = 1L
+    val testOutput2 = part2(testInput)
+    val expectedTestOutput2 = 46L
     check(testOutput2 == expectedTestOutput2) {
         "Part 2 Tests: Expected $expectedTestOutput2, got $testOutput2"
     }
-    part2(input).println()
+    println("Test Part 2 passed")
+    val input2 = readInput("Day05_2")
+    part2(input2).println()
 }
