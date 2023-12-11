382f9c58c4dc47882b4462cc1b68525d95083459
Robin Mayerhofer <32525368+rm98@users.noreply.github.com>
2023-12-11 07:15:58 +0100

    Day11 Part 2

--- src/Day11.kt
+++ src/Day11.kt
@@ -15,8 +15,6 @@ fun main() {
             input.all { line -> line[index] == '.' }
         }.toSet()
 
-        println("horizontalIndicesToExpand: $horizontalIndicesToExpand")
-
         return verticallyExpanded.map { line ->
             line.flatMapIndexed { index, char ->
                 if (index in horizontalIndicesToExpand) {
@@ -26,6 +24,18 @@ fun main() {
                 }
             }
         }.map { it.joinToString("") }
+    }
+
+    fun expandUniverse2(input: List<String>): Pair<Set<Int>, Set<Int>> {
+        val verticalIndicesToExpand = input.indices.filter { index ->
+            input[index].all { char -> char == '.' }
+        }.toSet()
+
+        val horizontalIndicesToExpand = input[0].indices.filter { index ->
+            input.all { line -> line[index] == '.' }
+        }.toSet()
+
+        return Pair(verticalIndicesToExpand, horizontalIndicesToExpand)
     }
 
     fun part1(input: List<String>): Int {
@@ -48,11 +58,43 @@ fun main() {
         }.sum()
     }
 
-
-    fun part2(input: List<String>): Int =
-            input.sumOf {
-                it.length
+    fun createRange(a: Int, b: Int): IntRange =
+            if (a < b) {
+                a..b
+            } else {
+                b..a
             }
+
+    fun part2(input: List<String>, expansionFactor: Long): Long {
+        val universe = input
+        val (verticalIndicesToExpand, horizontalIndicesToExpand) = expandUniverse2(input)
+        val galaxies: Set<Pair<Int, Int>> = universe.flatMapIndexed { row: Int, line: String ->
+            line.mapIndexedNotNull { column, c ->
+                if (c == '#') {
+                    row to column
+                } else {
+                    null
+                }
+            }
+        }.toSet()
+
+        return galaxies.flatMapIndexed { i1, (y1, x1) ->
+            galaxies.mapIndexed { i2, (y2, x2) ->
+                if (i2 <= i1) { 0 }
+                else {
+                    val part1 = abs(y1 - y2) + abs(x1 - x2)
+
+                    val xRange = createRange(x1, x2)
+                    val yRange = createRange(y1, y2)
+
+                    val xCount = xRange.count { x -> x in horizontalIndicesToExpand }
+                    val yCount = yRange.count { y -> y in verticalIndicesToExpand }
+
+                    (part1 - xCount - yCount).toLong() + xCount*expansionFactor + yCount*expansionFactor
+                }
+            }
+        }.sum()
+    }
 
     test(
             "Part 1 Test 1",
@@ -65,11 +107,17 @@ fun main() {
     part1(input).println()
 
     test(
-            "Part 2 Test 1",
+            "Part 2 Test with expansion factor 10",
             "Day11_test",
-            ::part2,
-            1
+            { input -> part2(input, 10L) },
+            1030L,
     )
-    val input2 = readInput("Day11_2").filter(String::isNotBlank)
-    part2(input2).println()
+    test(
+            "Part 2 Test with expansion factor 10",
+            "Day11_test",
+            { input -> part2(input, 100L) },
+            8410L,
+    )
+    val input2 = readInput("Day11").filter(String::isNotBlank)
+    part2(input2, 1_000_000L).println()
 }
