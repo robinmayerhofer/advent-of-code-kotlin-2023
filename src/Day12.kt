3eaccb3bc3a0e600c3b7e93910176ddec9a1d18f
Robin Mayerhofer <32525368+rm98@users.noreply.github.com>
2023-12-12 19:11:59 +0100

    Day12 Part 2

--- src/Day12.kt
+++ src/Day12.kt
@@ -5,7 +5,11 @@ private val OPERATIONAL = '.'
 private val DAMAGED = '#'
 private val UNKKNOWN = '?'
 
-private fun advanceRequirements(chars: Configuration, requirements: Requirements): Int {
+val memory = mutableMapOf<Pair<Requirements, Configuration>, Long>()
+
+private fun advanceRequirements(chars: Configuration, requirements: Requirements): Long {
+    memory[requirements to chars]?.let { return it }
+
     if (requirements.isEmpty()) {
         return 0
     }
@@ -15,27 +19,28 @@ private fun advanceRequirements(chars: Configuration, requirements: Requirements
         return 0
     }
 
-    val enoughDamagedSpringsInARow = ( 0..<expectedDamagedSprings).all {
-        chars.getOrNull(it) != OPERATIONAL
+    val notEnoughDamagedSpringsInARow = (0..<expectedDamagedSprings).any {
+        chars[it] == OPERATIONAL
     }
 
-    return if (!enoughDamagedSpringsInARow) {
-        0
-    } else if (expectedDamagedSprings == chars.size) {
-        if (requirements.size == 1) {
-            1
-        } else {
-            0
-        }
+    if (notEnoughDamagedSpringsInARow) {
+        return 0
+    }
+
+    return if (expectedDamagedSprings == chars.size) {
+        // At the end => requirements left?
+        if (requirements.size == 1) { 1 }
+        else { 0 }
     } else if (chars[expectedDamagedSprings] == DAMAGED) {
+        // There are too many damaged springs, after a requirement for damaged spring there must be an operational one
         0
     } else {
         // dropping 1 extra char allows to skip the branching when there is a unknown spring (it has to be operational)
-        solutions(chars.drop(expectedDamagedSprings+1), requirements.drop(1))
+        solutions(chars.drop(expectedDamagedSprings + 1), requirements.drop(1))
     }
 }
 
-private fun solutions(chars: Configuration, requirements: Requirements): Int {
+private fun solutions(chars: Configuration, requirements: Requirements): Long {
     if (chars.isEmpty()) {
         return when {
             requirements.isEmpty() -> 1
@@ -45,11 +50,12 @@ private fun solutions(chars: Configuration, requirements: Requirements): Int {
 
     return when (chars.first()) {
         OPERATIONAL -> solutions(chars.drop(1), requirements)
-        UNKKNOWN -> solutions(chars.drop(1), requirements) + advanceRequirements(chars, requirements)
-        DAMAGED -> advanceRequirements(chars, requirements)
+        DAMAGED -> advanceRequirements(chars, requirements).also { memory[requirements to chars] = it }
+        UNKKNOWN -> solutions(chars.drop(1), requirements) + advanceRequirements(chars, requirements).also { memory[requirements to chars] = it }
         else -> error("Invalid input")
     }
 }
+
 
 fun main() {
 
@@ -59,111 +65,128 @@ fun main() {
         return configuration.toList() to requirements.findAllNumbers()
     }
 
-    fun part1(input: List<String>): Int =
-        input.sumOf { line ->
+    fun part1(input: List<String>): Long {
+        memory.clear()
+        return input.sumOf { line ->
             val (config, req) = parseLine(line)
             solutions(config, req)
         }
+    }
 
-    fun part2(input: List<String>): Int =
-        input.sumOf { line ->
-            val (config, req) = parseLine(line)
+    fun part2(input: List<String>): Long {
+        memory.clear()
 
+        return input.sumOf { line ->
+            val (config, requirements) = parseLine(line)
+
+            val unfoldedConfig = (0..<5).joinToString("?") { config.joinToString("") }.toList()
+            val unfoldedRequirements = listOf(requirements, requirements, requirements, requirements, requirements).flatten()
             solutions(
-                (0..<5).joinToString("?") { config.joinToString("") }.toList(),
-                req,
-            )
+                unfoldedConfig,
+                unfoldedRequirements,
+            ).also {
+                "$it solutions for ${unfoldedConfig.joinToString("")} ${unfoldedRequirements.joinToString(",")}".println()
+            }
         }
+    }
 
-    test(
-        "Part 1 Test 1",
-        "???.### 1,1,3",
-        ::part1,
-        1
-    )
-    test(
-        "Part 1 Test 2",
-        ".??..??...?##. 1,1,3",
-        ::part1,
-        4
-    )
-    test(
-        "Part 1 Test 3",
-        "?#?#?#?#?#?#?#? 1,3,1,6",
-        ::part1,
-        1
-    )
-    test(
-        "Part 1 Test 4",
-        "????.#...#... 4,1,1",
-        ::part1,
-        1
-    )
-    test(
-        "Part 1 Test 5",
-        "????.######..#####. 1,6,5",
-        ::part1,
-        4
-    )
-    test(
-        "Part 1 Test 6",
-        "?###???????? 3,2,1",
-        ::part1,
-        10
-    )
-    testFile(
-        "Part 1 Test File",
-        "Day12_test",
-        ::part1,
-        21
-    )
+    fun testsPart1() {
+        test(
+            "Part 1 Test 1",
+            "???.### 1,1,3",
+            ::part1,
+            1
+        )
+        test(
+            "Part 1 Test 2",
+            ".??..??...?##. 1,1,3",
+            ::part1,
+            4
+        )
+        test(
+            "Part 1 Test 3",
+            "?#?#?#?#?#?#?#? 1,3,1,6",
+            ::part1,
+            1
+        )
+        test(
+            "Part 1 Test 4",
+            "????.#...#... 4,1,1",
+            ::part1,
+            1
+        )
+        test(
+            "Part 1 Test 5",
+            "????.######..#####. 1,6,5",
+            ::part1,
+            4
+        )
+        test(
+            "Part 1 Test 6",
+            "?###???????? 3,2,1",
+            ::part1,
+            10
+        )
+        testFile(
+            "Part 1 Test File",
+            "Day12_test",
+            ::part1,
+            21
+        )
+    }
 
-    test(
-        "Part 2 Test 1",
-        "???.### 1,1,3",
-        ::part2,
-        1
-    )
-    test(
-        "Part 2 Test 2",
-        ".??..??...?##. 1,1,3",
-        ::part2,
-        16384
-    )
-    test(
-        "Part 2 Test 3",
-        "?#?#?#?#?#?#?#? 1,3,1,6",
-        ::part2,
-        1
-    )
-    test(
-        "Part 2 Test 4",
-        "????.#...#... 4,1,1",
-        ::part2,
-        16
-    )
-    test(
-        "Part 2 Test 5",
-        "????.######..#####. 1,6,5",
-        ::part2,
-        2500
-    )
-    test(
-        "Part 2 Test 6",
-        "?###???????? 3,2,1",
-        ::part2,
-        506250
-    )
-    testFile(
-        "Part 2 Test File",
-        "Day12_test",
-        ::part2,
-        525152
-    )
-
+    testsPart1()
     val input = readInput("Day12").filter(String::isNotBlank)
     part1(input).println()
 
+    fun testsPart2() {
+        test(
+            "Part 2 Test 1",
+            "???.### 1,1,3",
+            ::part2,
+            1
+        )
+        test(
+            "Part 2 Test 2",
+            ".??..??...?##. 1,1,3",
+            ::part2,
+            16384
+        )
+        test(
+            "Part 2 Test 3",
+            "?#?#?#?#?#?#?#? 1,3,1,6",
+            ::part2,
+            1
+        )
+        test(
+            "Part 2 Test 4",
+            "????.#...#... 4,1,1",
+            ::part2,
+            16
+        )
+        test(
+            "Part 2 Test 5",
+            "????.######..#####. 1,6,5",
+            ::part2,
+            2500
+        )
+        test(
+            "Part 2 Test 6",
+            "?###???????? 3,2,1",
+            ::part2,
+            506250
+        )
+        testFile(
+            "Part 2 Test File",
+            "Day12_test",
+            ::part2,
+            525152
+        )
+    }
+
+    testsPart2()
     val input2 = readInput("Day12").filter(String::isNotBlank)
-    part2(input2).println()
+
+    part2(input2)
+        .println()
 }
