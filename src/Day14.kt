4bbcb978a2c03f4d268c72a7b9c7602056672841
Robin Mayerhofer <32525368+rm98@users.noreply.github.com>
2023-12-14 16:56:48 +0100

    Day10 Part 1

--- src/Day14.kt
+++ src/Day14.kt
@@ -1,21 +1,59 @@
 
 fun main() {
 
-    fun part1(input: List<String>): Int =
-        input.sumOf {
-            it.length
-        }
+    val ROUNDED_ROCK = 'O'
+    val CUBE_SHAPED_ROCK = '#'
+    val EMPTY_SPACE = '.'
 
-    fun part2(input: List<String>): Int =
-        input.sumOf {
-            it.length
+    fun Field.tiltNorth() {
+        val xs = this[0].indices
+        val ys = this.indices
+
+        xs.forEach { x ->
+            var nextShiftLocation: Int? = null
+            for (y in ys) {
+                when (this[y][x]) {
+                    ROUNDED_ROCK -> {
+                        if (nextShiftLocation != null) {
+                            this[y][x] = EMPTY_SPACE
+                            this[nextShiftLocation][x] = ROUNDED_ROCK
+                            nextShiftLocation += 1
+                        }
+                    }
+                    CUBE_SHAPED_ROCK -> nextShiftLocation = null
+                    EMPTY_SPACE -> {
+                        if (nextShiftLocation == null) {
+                            nextShiftLocation = y
+                        }
+                    }
+                }
+            }
         }
+    }
+
+    fun Field.calculateLoad(): Long {
+        val lines = size.toLong()
+
+        return mapIndexed { index, chars ->
+            val multiplier = lines - index
+            chars.count { it == ROUNDED_ROCK } * multiplier
+        }.sum()
+    }
+
+    fun part1(input: List<String>): Long {
+        val field = inputToField(input)
+        field.tiltNorth()
+        return field.calculateLoad()
+    }
+
+    fun part2(input: List<String>): Long =
+        TODO()
 
     testFile(
         "Part 1 Test 1",
         "Day14_test",
         ::part1,
-        1
+        136L
     )
 
     val input = readInput("Day14").filter(String::isNotBlank)
