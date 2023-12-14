d31137a0d318c6b084fd5e4104060a71d0ff820b
Robin Mayerhofer <32525368+rm98@users.noreply.github.com>
2023-12-14 16:46:42 +0100

    Day14 Preparation

---
+++ src/Day14.kt
@@ -0,0 +1,32 @@
+
+fun main() {
+
+    fun part1(input: List<String>): Int =
+        input.sumOf {
+            it.length
+        }
+
+    fun part2(input: List<String>): Int =
+        input.sumOf {
+            it.length
+        }
+
+    testFile(
+        "Part 1 Test 1",
+        "Day14_test",
+        ::part1,
+        1
+    )
+
+    val input = readInput("Day14").filter(String::isNotBlank)
+    part1(input).println()
+
+    testFile(
+        "Part 2 Test 1",
+        "Day14_test",
+        ::part2,
+        1
+    )
+    val input2 = readInput("Day14_2").filter(String::isNotBlank)
+    part2(input2).println()
+}
