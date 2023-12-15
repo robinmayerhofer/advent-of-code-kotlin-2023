7f8f7005f9851e4e6aaacd1eb95ea9c4ce54c568
Robin Mayerhofer <32525368+rm98@users.noreply.github.com>
2023-12-15 09:40:40 +0100

    Day15 Part 1 + 2

--- src/Day15.kt
+++ src/Day15.kt
@@ -1,31 +1,72 @@
 fun main() {
 
-    fun part1(input: List<String>): Int =
-        input.sumOf {
-            it.length
+    fun String.hash(): Int =
+        fold(initial = 0) { acc: Int, c: Char ->
+            ((acc + c.code) * 17) % 256
         }
 
-    fun part2(input: List<String>): Int =
-        input.sumOf {
-            it.length
+    fun part1(input: String): Int =
+        input
+            .split(",")
+            .sumOf(String::hash)
+
+    fun printBoxes(boxes: Array<MutableList<String>>) {
+        boxes.mapIndexed { boxNumber, slots ->
+            val str = slots.joinToString(" ") { "[${it.replace("=", " ")}]" }
+            if (str.isNotEmpty()) {
+                println("Box ${boxNumber}: $str")
+            }
         }
+    }
 
-    testFile(
-        "Part 1 Test 1",
-        "Day14_test",
-        ::part1,
-        1
-    )
+    fun part2(input: String): Long {
+        val boxes = Array(256) {
+            mutableListOf<String>()
+        }
+        input
+            .split(",")
+            .forEach { instruction ->
+                if ("=" in instruction) {
+                    val id = instruction.split("=")[0]
+                    val idHash = id.hash()
+                    val index = boxes[idHash].indexOfFirst { it.startsWith("$id=") }
+                    if (index >= 0) {
+                        boxes[idHash][index] = instruction
+                    } else {
+                        boxes[idHash].add(instruction)
+                    }
+                } else if ("-" in instruction) {
+                    val id = instruction.split("-")[0]
+                    val idHash = id.hash()
+                    boxes[idHash].removeAll { it.startsWith("$id=") }
+                } else {
+                    error("?")
+                }
 
-    val input = readInput("Day14").filter(String::isNotBlank)
-    part1(input).println()
+                println("After \"$instruction\":")
+                printBoxes(boxes)
+                println()
+            }
 
-    testFile(
-        "Part 2 Test 1",
-        "Day14_test",
-        ::part2,
-        1
-    )
-    val input2 = readInput("Day14_2").filter(String::isNotBlank)
-    part2(input2).println()
+        return boxes
+            .flatMapIndexed { box, slots ->
+                slots.mapIndexed { slot, instruction ->
+                    val value = (box + 1) * (slot + 1) * (instruction.split("=")[1].toLong())
+                    println("${instruction}: $value")
+                    value
+                }
+            }.sum()
+    }
+
+    part1("HASH").also { check(it == 52) { "Result was not 52 but $it" } }
+
+    val input = readInput("Day15").filter(String::isNotBlank)
+    part1(input[0]).println()
+
+    part2("rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7")
+        .also { check(it == 145L) }
+        .println()
+    part2(input[0])
+        .also { check(it == 296921L) { "Wrong result $it, should be 296921" } }
+        .println()
 }
