286c1501c77e8f6466718f274c2bcf60e4b0419b
Robin Mayerhofer <32525368+robinmayerhofer@users.noreply.github.com>
2023-12-17 18:07:09 +0100

    Day17 Enable P1 again

--- src/Day17.kt
+++ src/Day17.kt
@@ -35,96 +35,90 @@ fun main() {
     fun Position.travel(direction: Dir): Position =
         copy(column = column + direction.deltaX, row = row + direction.deltaY)
 
-//    fun aStarThisFieldP1(field: DigitField): Int {
-//        val priorityQueue = PriorityQueue<CacheValue>()
-//        val cache: MutableMap<Pair<Position, Dir>, CacheValue> = mutableMapOf()
-//
-//        val startPosition = Position(
-//            row = 0,
-//            column = 0,
-//        )
-//        val endPosition = Position(
-//            row = field.size - 1,
-//            column = field[0].size - 1,
-//        )
-//
-//        priorityQueue.add(
-//            CacheValue(
-//                position = Position(0, 0),
-//                costUntilPreviousPosition = -field[0][0],
-//                lastDirection = null,
-//            )
-//        )
-//        while (priorityQueue.isNotEmpty()) {
-//            val current = priorityQueue.poll()
-//            println("Trying: $current")
-//
-//            if (!field.isValidPosition(current.position)) {
-//                println("Skipping invalid.")
-//                continue
-//            }
-//            if (current.position == startPosition && current.lastDirection != null) {
-//                println("Skipping start position after circle.")
-//                continue
-//            }
-//
-//            val costOfPosition = field.elementAt(current.position)
-//            // End condition
-//            if (current.position == endPosition) {
-//                println("Found end position.")
-//                return current.costUntilPreviousPosition + costOfPosition
-//            }
-//
-//            if (current.lastDirection != null) {
-//                val cachedValue: CacheValue? = cache[current.position to current.lastDirection.dir]
-//                if (cachedValue != null) {
-//                    if (cachedValue.lastDirection != null) {
-//                        if (cachedValue.lastDirection.amount <= current.lastDirection.amount) {
-//                            if (cachedValue.costUntilPreviousPosition <= current.costUntilPreviousPosition) {
-//                                println("Found better entry in cache: $cachedValue")
-//                                continue
-//                            }
-//                        }
-//                    }
-//                }
-//
-//                cache[current.position to current.lastDirection.dir] = current
-//            }
-//
-//            val forbiddenDirections =
-//                current.lastDirection?.dir?.reverse()?.let { mutableListOf(it) } ?: mutableListOf()
-//            if (current.lastDirection?.amount == 3) {
-//                forbiddenDirections.add(current.lastDirection.dir)
-//            } // should not go forward
-//
-//            for (direction in Dir.entries) {
-//                if (direction in forbiddenDirections) {
-//                    continue
-//                }
-//
-//                val newLastDirection = LastDirection(
-//                    dir = direction,
-//                    amount = if (direction == current.lastDirection?.dir) {
-//                        current.lastDirection.amount + 1
-//                    } else {
-//                        1
-//                    }
-//                )
-//
-//                val next = CacheValue(
-//                    position = current.position.travel(direction),
-//                    costUntilPreviousPosition = current.costUntilPreviousPosition + costOfPosition,
-//                    lastDirection = newLastDirection
-//                )
-//                println("Adding: $next")
-//                priorityQueue.add(next)
-//            }
-//
-//            println()
-//        }
-//
-//        error("Oops, found no end")
-//    }
+    fun dijkstraPart1(field: DigitField): Int {
+        val priorityQueue = PriorityQueue<CacheValue>()
+        val cache: MutableMap<Pair<Position, Dir>, CacheValue> = mutableMapOf()
+
+        val startPosition = Position(
+            row = 0,
+            column = 0,
+        )
+        val endPosition = Position(
+            row = field.size - 1,
+            column = field[0].size - 1,
+        )
+
+        priorityQueue.add(
+            CacheValue(
+                position = Position(0, 0),
+                costUntilPreviousPosition = -field[0][0],
+                lastDirection = null,
+                previous = null,
+            )
+        )
+        while (priorityQueue.isNotEmpty()) {
+            val current = priorityQueue.poll()
+
+            if (!field.isValidPosition(current.position)) {
+                continue
+            }
+            if (current.position == startPosition && current.lastDirection != null) {
+                continue
+            }
+
+            val costOfPosition = field.elementAt(current.position)
+            if (current.position == endPosition) {
+                return current.costUntilPreviousPosition + costOfPosition
+            }
+
+            if (current.lastDirection != null) {
+                val cachedValue: CacheValue? = cache[current.position to current.lastDirection.dir]
+                if (cachedValue != null) {
+                    if (cachedValue.lastDirection != null) {
+                        if (cachedValue.lastDirection.amount <= current.lastDirection.amount) {
+                            if (cachedValue.costUntilPreviousPosition <= current.costUntilPreviousPosition) {
+                                continue
+                            }
+                        }
+                    }
+                }
+
+                cache[current.position to current.lastDirection.dir] = current
+            }
+
+            val forbiddenDirections = current.lastDirection?.dir?.reverse()?.let { mutableListOf(it) }
+                ?: mutableListOf()
+            if (current.lastDirection?.amount == 3) {
+                // should not go forward
+                forbiddenDirections.add(current.lastDirection.dir)
+            }
+
+            for (direction in Dir.entries) {
+                if (direction in forbiddenDirections) {
+                    continue
+                }
+
+                val newLastDirection = LastDirection(
+                    dir = direction,
+                    amount = if (direction == current.lastDirection?.dir) {
+                        current.lastDirection.amount + 1
+                    } else {
+                        1
+                    }
+                )
+
+                val next = CacheValue(
+                    position = current.position.travel(direction),
+                    costUntilPreviousPosition = current.costUntilPreviousPosition + costOfPosition,
+                    lastDirection = newLastDirection,
+                    previous = current,
+                )
+                priorityQueue.add(next)
+            }
+        }
+
+        error("Oops, found no end")
+    }
 
     fun CacheValue.printPath() {
         val positions = mutableListOf<Position>()
@@ -137,7 +131,7 @@ fun main() {
         positions.reversed().println()
     }
 
-    fun aStarThisFieldP2(field: DigitField): Int {
+    fun dijkstraPart2(field: DigitField): Int {
         val priorityQueue = PriorityQueue<CacheValue>()
         val cache: MutableMap<Pair<Position, Dir>, CacheValue> = mutableMapOf()
 
@@ -164,20 +158,16 @@ fun main() {
 
         while (priorityQueue.isNotEmpty()) {
             val current = priorityQueue.poll()
-//            println("Trying: $current")
             if (!field.isValidPosition(current.position)) {
-//                println("Skipping invalid position.")
                 continue
             }
             if (current.position == startPosition && current.lastDirection != null) {
-//                println("Skipping start position after circle.")
                 continue
             }
 
             val costOfPosition = field.elementAt(current.position)
-            // End condition
             if (current.position == endPosition && (current.lastDirection?.amount ?: 0) >= 4) {
-//                println("Found end position.")
+                println("Found path to end:")
                 current.printPath()
                 return current.costUntilPreviousPosition + costOfPosition
             }
@@ -191,7 +181,6 @@ fun main() {
                             (cachedValue.lastDirection.amount >= 4 && cachedValue.lastDirection.amount <= current.lastDirection.amount)
                         ) {
                             if (cachedValue.costUntilPreviousPosition <= current.costUntilPreviousPosition) {
-//                                println("Found better entry in cache: $cachedValue.")
                                 continue
                             }
                         }
@@ -221,8 +210,6 @@ fun main() {
                 allowedDirections.addAll(Dir.entries.toList())
             }
 
-//            println("Allowed directions: $allowedDirections")
-
             for (direction in allowedDirections) {
                 val newLastDirection = LastDirection(
                     dir = direction,
@@ -240,7 +227,6 @@ fun main() {
                     previous = current,
                 )
                 if (field.isValidPosition(next.position)) {
-//                    println("Adding: $next")
                     priorityQueue.add(next)
                 }
             }
@@ -256,56 +242,57 @@ fun main() {
     }
 
 
-//    fun part1(input: List<String>): Int {
-//        val field = inputToDigitField(input)
-//        return aStarThisFieldP1(field)
-//    }
+    fun part1(input: List<String>): Int {
+        val field = inputToDigitField(input)
+        return dijkstraPart1(field)
+    }
 
     fun part2(input: List<String>): Int {
         val field = inputToDigitField(input)
-        return aStarThisFieldP2(field)
+        return dijkstraPart2(field)
     }
 
-//    test(
-//        "Part 1 Test 1",
-//        """
-//            011
-//            111
-//            111
-//        """.trimIndent(),
-//        ::part1,
-//        4,
-//    )
-//
-//    testFile(
-//        "Part 1 Test 1",
-//        "Day17_test",
-//        ::part1,
-//        102,
-//    )
-//
-//    println("Passed tests for Part 1")
-    val input = readInput("Day17").filter(String::isNotBlank)
-//    part1(input).println()
+    test(
+        "Part 1 Test 1",
+        """
+            011
+            111
+            111
+        """.trimIndent(),
+        ::part1,
+        4,
+    )
 
-//    testFile(
-//        "Part 2 Test 1",
-//        "Day17_test",
-//        ::part2,
-//        94,
-//    )
-//    test(
-//        "Part 2 Test 1",
-//        """
-//            011111111111
-//            999999999991
-//            999999999991
-//            999999999991
-//            999999999991
-//        """.trimIndent(),
-//        ::part2,
-//        71,
-//    )
+    testFile(
+        "Part 1 Test 1",
+        "Day17_test",
+        ::part1,
+        102,
+    )
+
+    println("Passed tests for Part 1")
+    val input = readInput("Day17").filter(String::isNotBlank)
+    measure { part1(input) }
+        .println()
+
+    testFile(
+        "Part 2 Test 1",
+        "Day17_test",
+        ::part2,
+        94,
+    )
+    test(
+        "Part 2 Test 1",
+        """
+            011111111111
+            999999999991
+            999999999991
+            999999999991
+            999999999991
+        """.trimIndent(),
+        ::part2,
+        71,
+    )
 
     measure { part2(input) }
         .println()
