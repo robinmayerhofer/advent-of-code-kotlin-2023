e58ce14815bb60287840aaae944c322f12bcbd9e
Robin Mayerhofer <32525368+robinmayerhofer@users.noreply.github.com>
2023-12-19 07:34:57 +0100

    Day19 init

--- src/Day19.kt
+++ src/Day19.kt
@@ -1,6 +1,24 @@
 fun main() {
 
-    fun part1(input: List<String>): Int =
+    data class WorkflowSteps()
+
+    data class Workflow(
+        val name: String
+        val steps: List<WorkflowSteps>
+    ) {
+        fun pro
+    }
+
+    fun String.toWorkflow() {
+
+    }
+
+    fun part1(input: List<String>): Int {
+        val rawWorkflows = input.takeWhile { it.isNotEmpty() }
+        val rawParts = input.drop(rawWorkflows.size).filter { it.isNotEmpty() }
+
+
+    }
         input.sumOf {
             it.length
         }
@@ -14,18 +32,18 @@ fun main() {
         "Part 1 Test 1",
         "Day19_test",
         ::part1,
-        1
+        19114
     )
 
-    val input = readInput("Day19").filter(String::isNotBlank)
-    part1(input).println()
-
-    testFile(
-        "Part 2 Test 1",
-        "Day19_test",
-        ::part2,
-        1
-    )
-    val input2 = readInput("Day19_2").filter(String::isNotBlank)
-    part2(input2).println()
+//    val input = readInput("Day19").filter(String::isNotBlank)
+//    part1(input).println()
+//
+//    testFile(
+//        "Part 2 Test 1",
+//        "Day19_test",
+//        ::part2,
+//        1
+//    )
+//    val input2 = readInput("Day19_2").filter(String::isNotBlank)
+//    part2(input2).println()
 }
