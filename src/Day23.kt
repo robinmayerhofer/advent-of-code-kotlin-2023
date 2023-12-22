fe0912b19d3a472e28c5221d7022b2152f4a99fe
Robin Mayerhofer <32525368+robinmayerhofer@users.noreply.github.com>
2023-12-22 17:01:12 +0100

    Day23 Preparation

---
+++ src/Day23.kt
@@ -0,0 +1,34 @@
+fun main() {
+    shouldLog = true
+
+    fun part1(input: List<String>): Int =
+        input.sumOf {
+            it.length
+        }
+
+    fun part2(input: List<String>): Int =
+        input.sumOf {
+            it.length
+        }
+
+    testFile(
+        "Part 1 Test 1",
+        "Day23_test",
+        ::part1,
+        1,
+        filterBlank = false,
+    )
+
+    val input = readInput("Day23").filter(String::isNotBlank)
+    part1(input).println()
+
+//    testFile(
+//        "Part 2 Test 1",
+//        "Day23_test",
+//        ::part2,
+//        1,
+//        filterBlank = false,
+//    )
+//    val input2 = readInput("Day23").filter(String::isNotBlank)
+//    part2(input2).println()
+}
