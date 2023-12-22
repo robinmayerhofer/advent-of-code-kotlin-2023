9583785dc82e25d074253f33d5277a1632668d32
Robin Mayerhofer <32525368+robinmayerhofer@users.noreply.github.com>
2023-12-22 08:45:36 +0100

    Day22 Part 1 WIP

--- src/Day22.kt
+++ src/Day22.kt
@@ -1,34 +1,175 @@
-fun main() {
-    shouldLog = true
+import kotlin.math.max
+import kotlin.math.min
 
-    fun part1(input: List<String>): Int =
-        input.sumOf {
-            it.length
+fun main() {
+    data class Position3D(
+        val x: Int,
+        val y: Int,
+        val z: Int,
+    ) : Comparable<Position3D> {
+
+        override fun compareTo(other: Position3D): Int {
+            if (z != other.z) {
+                return z - other.z
+            }
+            if (y != other.y) {
+                return y - other.y
+            }
+            return x - other.x
         }
+    }
+
+    fun minPosition(a: Position3D, b: Position3D): Position3D =
+        if (a < b) {
+            a
+        } else {
+            b
+        }
+
+    fun IntRange.intersects(other: IntRange): Boolean {
+        if (this.first >= other.first && this.first <= other.last) {
+            return true
+        } else if (other.first >= this.first && other.first <= this.last) {
+            return true
+        } else {
+            return false
+        }
+    }
+
+    data class Brick(
+        val start: Position3D,
+        val end: Position3D,
+    ) : Comparable<Brick> {
+
+        val xRange: IntRange = min(start.x, end.x)..max(start.x, end.x)
+        val yRange: IntRange = min(start.y, end.y)..max(start.y, end.y)
+        val zRange: IntRange = min(start.z, end.z)..max(start.z, end.z)
+
+        fun intersectsXY(other: Brick): Boolean =
+            xRange.intersects(other.xRange) && yRange.intersects(other.yRange)
+
+
+        fun intersects(other: Brick): Boolean =
+            xRange.intersects(other.xRange) && yRange.intersects(other.yRange) && zRange.intersects(other.zRange)
+
+        fun supports(other: Brick): Boolean =
+            if (zRange.first == other.zRange.last - 1) {
+                intersectsXY(other)
+            } else {
+                false
+            }
+
+        override fun compareTo(other: Brick): Int {
+            val min = minPosition(start, end)
+            val otherMin = minPosition(other.start, other.end)
+
+            return min.compareTo(otherMin)
+        }
+    }
+
+    fun String.toBrick(): Brick {
+        val (start, end) = this.split("~")
+            .map { it.findAllNumbers() }
+
+        return Brick(
+            start = Position3D(
+                x = start[0],
+                y = start[1],
+                z = start[2],
+            ),
+            end = Position3D(
+                x = end[0],
+                y = end[1],
+                z = end[2],
+            ),
+        )
+    }
+
+    fun part1(input: List<String>): Int {
+        val givenSortedBricks = input
+            .map(String::toBrick)
+            .sorted()
+
+        println("Found bricks")
+        givenSortedBricks.asReversed().forEach(::println)
+
+        val droppedBricks = mutableListOf<Brick>()
+        val minZ = 1
+
+        for (brick in givenSortedBricks) {
+            val blockingBrick = droppedBricks.firstOrNull { it.intersectsXY(brick) }
+
+            val zDrop = if (blockingBrick == null) {
+                brick.zRange.first - minZ
+            } else {
+                brick.zRange.first - blockingBrick.zRange.last - 1
+            }
+            println("zDrop: $zDrop")
+            droppedBricks.add(
+                index = 0,
+                element = Brick(
+                    start = brick.start.copy(z = brick.start.z - zDrop),
+                    end = brick.end.copy(z = brick.end.z - zDrop),
+                )
+            )
+        }
+        println("Dropped bricks")
+        droppedBricks.forEach(::println)
+
+        // highest droppedBrick is at the start
+        val indexToSupportedIndices: Map<Int, List<Int>> = droppedBricks.mapIndexed { index, brick ->
+            index to droppedBricks.take(index).mapIndexedNotNull { index, other ->
+                if (brick.supports(other)) {
+                    index
+                } else {
+                    null
+                }
+            }
+        }.toMap()
+
+        val canRemove = MutableList(droppedBricks.size) { true }
+
+        val indexToSupportingIndices =
+            indexToSupportedIndices.entries.fold(mutableMapOf()) { acc: MutableMap<Int, MutableSet<Int>>, (supportingIndex: Int, supportedIndices: List<Int>) ->
+                supportedIndices.forEach { supportedIndex ->
+                    val supportedBySet = acc.getOrDefault(supportedIndex, mutableSetOf())
+                    supportedBySet.add(supportingIndex)
+                    acc[supportedIndex] = supportedBySet
+                }
+                acc
+            }
+
+        indexToSupportingIndices.values.forEach { supportingIndices ->
+            if (supportingIndices.size == 1) {
+                canRemove[supportingIndices.first()] = false
+            }
+        }
+
+        return canRemove.count { it }
+    }
 
     fun part2(input: List<String>): Int =
         input.sumOf {
             it.length
         }
 
+    shouldLog = true
     testFile(
         "Part 1 Test 1",
         "Day22_test",
         ::part1,
-        1,
-        filterBlank = false,
+        5,
     )
 
     val input = readInput("Day22").filter(String::isNotBlank)
     part1(input).println()
 
-    testFile(
-        "Part 2 Test 1",
-        "Day22_test",
-        ::part2,
-        1,
-        filterBlank = false,
-    )
-    val input2 = readInput("Day22").filter(String::isNotBlank)
-    part2(input2).println()
+//    testFile(
+//        "Part 2 Test 1",
+//        "Day22_test",
+//        ::part2,
+//        1,
+//    )
+//    val input2 = readInput("Day22").filter(String::isNotBlank)
+//    part2(input2).println()
 }
